4a99cccf06672f7a7db82f22049c747b318428fa
felis <guillaume.roucheux@tutanota.com>
2024-11-12 22:16:36 +0100

    Commeit v1

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# update_ip_codovore
+# Update ip pour serveur autohebergé
+
+## Utilité:
+
+Sert à mettre à jour automatiquement l'ip de la zone dns pointant vers un serveur personnel, via l'api Infomaniak. En effet,
+les fournisseurs d'acces changent l'ip externe des boxes. Ce programme permet de passer outre simplement cet inconvenient quand on
+heberge un serveur chez sois
+
+## Utilisation:
+
+Pour l'utiliser il faut: 
+
+- un nom de domaine enregistré chez infomaniak
+- Une clé api Infomaniak
+- l'id de l'enregistrement dns à modifier.
+
+## Fonctionnement
+
+Le programme va chercher l'ip locale via un site d'affichage d'ip, et l'ip enregistrée sur infomaniak. Si les ip sont 
+différentes, il met à jour celle sur infomaniak
---
+++ main.py
@@ -0,0 +1,78 @@
+import requests
+import json
+import logging
+
+logger = logging.getLogger("fix_ip")
+logging.basicConfig(format='%(asctime)s - %(levelname)s: %(message)s', filename='fix_ip.log', encoding='utf-8',
+                    level=logging.DEBUG)
+url_ipfy = 'https://api.ipify.org?format=json'
+zoneId = < Number: id de la zone dns à modifier >
+bearer_token = "<String: clé api >"
+domainName = "< String: nom de domaine cible >"
+
+
+def get_registred_ip():
+    headers = {"Authorization": f"Bearer {bearer_token}"}
+    url = "https://api.infomaniak.com/2/zones/codovore.fr/records"
+    try:
+        response = requests.get(url, headers=headers)
+        datas = response.json()
+        for i in datas['data']:
+            if i['id'] == zoneId:
+                return i['target']
+
+    except requests.exceptions.RequestException as e:
+        logging.critical(f'Error: {e}')
+        return False
+
+
+def get_public_ip():
+    try:
+        response = requests.get(url_ipfy)
+        response.raise_for_status()
+        return response.json()['ip']
+
+    except requests.exceptions.RequestException as e:
+        logging.critical(f'Error getting public IP: {e}')
+        return False
+
+
+def fixBadIp(newIp):
+    url_put = f"https://api.infomaniak.com/2/zones/{domainName}/records/{zoneId}"
+    headers = {
+        'Authorization': f"Bearer {bearer_token}",
+        'Content-Type': 'application/json',
+    }
+    data = json.dumps({
+        "target": newIp,
+    })
+    try:
+        req = requests.request("PUT", url=url_put, data=data, headers=headers)
+        res = req.json()
+
+    except requests.exceptions.RequestException as e:
+        logger.critical(f"Error: {e}")
+        return False
+    return res
+
+
+infomaniakIp = get_registred_ip()
+actualIp = get_public_ip()
+
+if infomaniakIp is False or actualIp is False:
+    exit()
+
+if actualIp != infomaniakIp:
+    logging.info(f'Ip has changed: actual IP{actualIp} - registred ip: {infomaniakIp}. Trying to fix it.')
+    try:
+        fixBadIp(actualIp)
+        print('yo')
+
+    except Exception as e:
+        print(e)
+
+    else:
+        logging.info(f'Ip updated. New ip: {actualIp}\n')
+
+else:
+    logging.info("IP is un to date, nothing to change \n")
